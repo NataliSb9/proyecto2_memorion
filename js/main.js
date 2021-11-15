a8e0f274abdfe9aff31f3f7cf17d5f270e0f7fe1
NataliSb9 <nsainzbarroso@gmail.com>
2021-11-15 15:35:35 +0100

    funcion mostrar cartas

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const urlImg = "https://hipertextual.com/wp-content/uploads/2017/07/rick-morty-.jpg"
+
+window.onload = crearDisposicionTarjeta()
+
+function crearDisposicionTarjeta(){
+    for(let i=0; i<= 15; i++){
+        let card = document.createElement("div");
+        card.id = `card${i}`
+        card.innerHTML = 
+        `<div class="card" style="width: 15rem; height:16rem;">
+            <img class="card-img-top border" src="${urlImg}" alt="Card image cap">
+        </div>`
+        
+        document.getElementById("tablero").appendChild(card)
+    }
+}
