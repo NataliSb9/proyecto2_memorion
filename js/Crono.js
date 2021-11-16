34e6466cede00eb036820268ec650c0edf3118b0
javivigarcia <13ajavi@gmail.com>
2021-11-16 18:15:11 +0100

    Cronometro implementado con parametro registro listo para guardarlo

---
+++ js/Crono.js
@@ -0,0 +1,50 @@
+window.onload = init;
+// Funcion para iniciar en cuanto s eabra la pagina, añadimos que al hacer click se activen las funciones correspondientes e inicializamos rn 0 el crono:
+function init(){
+    
+    document.querySelector("#botonEmpezar").addEventListener("click",cronometrar);
+    //document.querySelector(".stop").addEventListener("click",parar);
+    //document.querySelector(".reiniciar").addEventListener("click",reiniciar);
+    h = 0;
+    m = 0;
+    s = 0;
+    document.getElementById("hms").innerHTML="00:00:00";
+}     
+// funcion para iniciar el cronometro:    
+function cronometrar(){
+    setTimeout(function(){
+    escribir();
+    id = setInterval(escribir,1000);
+    document.querySelector("#botonEmpezar").removeEventListener("click",cronometrar);},3000)
+}
+
+// funcion para imprimir el cambio de tiempo en pantalla, teniendo en cuenta que las decimas y centesimas se tienen que definir de distinta manera ( No se admiten numeros de dos cifras en js)
+function escribir(){
+    let hAux, mAux, sAux;
+    s++;
+    if (s>59){m++;s=0;}
+    if (m>59){h++;m=0;}
+    if (h>24){h=0;}
+
+    if (s<10){sAux="0"+s;}else{sAux=s;}
+    if (m<10){mAux="0"+m;}else{mAux=m;}
+    if (h<10){hAux="0"+h;}else{hAux=h;}
+
+    let registro= hAux + ":" + mAux + ":" + sAux; 
+
+    document.getElementById("hms").innerHTML = registro; 
+}
+/*Para parar el crono y poder continuar cuando queramos
+function parar(){
+    clearInterval(id);
+    document.querySelector(".start").addEventListener("click",cronometrar);
+
+}
+
+// Funcion para reiniciar el conteo:
+function reiniciar(){
+    clearInterval(id);
+    document.getElementById("hms").innerHTML="00:00:00";
+    h=0;m=0;s=0;
+    document.querySelector(".start").addEventListener("click",cronometrar); 
+}*/
